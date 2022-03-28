810be5e2b2d78213ad366a668d2dbd41f3118683
Darren_Hsu <bthd0404@gmail.com>
2022-03-29 00:14:31 +0800

    create server

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const http = require('http');
 const path = require('path');
 const cors = require('cors');
 const express = require('express');
 const error = require('http-errors');
-
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -17,14 +15,17 @@ mongoose
 .connect(DB)
 .then(() => console.log('資料庫連接成功'));
 
-var PostsRouter = require('./routes/posts');
+var indexRouter = require('./routes/index');
+var postsRouter = require('./routes/posts');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.use('/posts', PostsRouter);
+app.use('/', indexRouter);
+app.use('/posts', postsRouter);
 
 app.use((req, res, next) =>{
   res.status(404).json({
@@ -44,5 +45,4 @@ app.use((err, req, res, next) =>{
   });
 });
 
-const server = http.createServer(app);
-server.listen(process.env.PORT || 8080)
+module.exports = app;
